83c0a7ab3aa60807a3b39c77bf0de7e24bba8022
bart400 <bartbien88@gmail.com>
2014-12-03 19:50:42 +0100

    init - tomcat 8 websocket with annotation configuration.

---
+++ src/main/java/connection/WebSocketConnection.java
@@ -0,0 +1,32 @@
+package connection;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(value = "/websocket/connect")
+public class WebSocketConnection {
+
+	@OnOpen
+	public void onOpen(Session session) {
+		System.out.println("onOpen");
+	}
+
+	@OnMessage
+	public void onTextMessage(String message) {
+
+	}
+
+	@OnClose
+	public void onClose() {
+		System.out.println("onClose");
+	}
+
+	@OnError
+	public void onError(Throwable t) throws Throwable {
+
+	}
+}
