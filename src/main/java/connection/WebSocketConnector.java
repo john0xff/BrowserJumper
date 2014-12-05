47431f0b6f04a9ffb1b1d0af33f15824bd863618
bart400 <bartbien88@gmail.com>
2014-12-05 18:40:04 +0100

    tomcat 8 snake with websocket

--- src/main/java/connection/WebSocketConnector.java
+++ src/main/java/connection/WebSocketConnector.java
@@ -1,7 +1,10 @@
 package connection;
 
 import java.io.IOException;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
+import javax.swing.JButton;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
@@ -9,11 +12,17 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import org.apache.tomcat.websocket.server.WsRemoteEndpointImplServer;
+
+// To check impl
+// WsRemoteEndpointImplServer s;
+
 @ServerEndpoint(value = "/websocket/connector")
 public class WebSocketConnector
 {
 	private Session session;
-
+	JButton a;
+	
 	@OnOpen
 	public void onOpen(Session session)
 	{
@@ -26,19 +35,28 @@ public class WebSocketConnector
 	{
 		System.out.println("onTextMessage -> " + message);
 
-		try
-		{
-			session.getBasicRemote().sendText("pong to client");
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
+//		try
+//		{
+//			session.getBasicRemote().sendText("pong to client");
+//		}
+//		catch (IOException e)
+//		{
+//			e.printStackTrace();
+//		}
 	}
 
 	@OnClose
 	public void onClose()
 	{
+		try
+		{
+			this.session.close();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("onClose");
 	}
 
